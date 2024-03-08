873802cdb33794b2280c974ca797d58fa145fdb8
Ivan Kostov <kostovster@gmail.com>
2024-03-08 12:26:14 +0200

    update: governance content

--- src/views/GovernanceView.vue
+++ src/views/GovernanceView.vue
@@ -87,9 +87,9 @@ import PlusSmallIcon from '@/assets/icons/plus-small.svg';
 
 
 const stats = [
-  {description: "maintaining the network's integrity", value: '50 Validators' },
-  {description: 'actively engaged in governance', value: '3,454 Delegators' },
-  {description: '', value: '' },
+  {description: "Maintain the Nolus blockchain integrity", value: '50 Validators' },
+  {description: 'Actively engage in governing the protocol\'s development', value: '3,454 Delegators' },
+  {description: 'NLS tokens secure the Nolus network', value: '272M' },
 ]
 
 let myCanvas = ref(null);
