06ebaeae8e1a44b04d484c32ebbcbfc10b1661fb
Ivan Kostov <kostovster@gmail.com>
2024-03-18 12:40:55 +0200

    update: contents

--- src/views/HomePartials/Hero.vue
+++ src/views/HomePartials/Hero.vue
@@ -69,7 +69,7 @@ import VideoModal from '@/components/modals/VideoModal.vue';
 import videoSrc from '@/assets/videos/header.mp4';
 
 const stats = [
-  { id: 1, subtitle: 'Tx Volume', description: 'Transactional value channeled through Nolus', value: '$100M+' },
+  { id: 1, subtitle: 'Tx Volume', description: 'Transactional value channeled through Nolus', value: '$100,000,000' },
   { id: 2, subtitle: 'Value Locked', description: 'Liquidity locked across 25 assets distributed on 3 chains', value: '$1,170,869' },
   { id: 3, subtitle: 'Earn', description: 'Annual percentage yield on lent assets, sustained sustainably', value: '9.24%' },
 ]
