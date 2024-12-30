e6720297cb0ef87e1314bc13f9cbe604293850c4
kostovster <kostovster@gmail.com>
2024-12-30 12:41:14 +0200

    add: hydro as liquidity hub

--- src/views/HomePartials/Hubs.vue
+++ src/views/HomePartials/Hubs.vue
@@ -52,6 +52,7 @@
 import NolusContainer from "@/components/NolusContainer.vue";
 import osmosisImage from "../../assets/images/hubs/osmosis.svg";
 import astroportImage from "../../assets/images/hubs/astroport.svg";
+import hydroImage from "../../assets/images/hubs/hydro.svg";
 
 const hubs = [
   {
@@ -69,6 +70,14 @@ const hubs = [
     comingSoon: false,
     image: astroportImage,
     link: "https://astroport.finance/"
-  }
+  },
+  {
+    id: 3,
+    shortName: "Neutron",
+    fullName: "Hydro",
+    comingSoon: false,
+    image: hydroImage,
+    link: "https://hydro.cosmos.network/"
+  },
 ];
 </script>
