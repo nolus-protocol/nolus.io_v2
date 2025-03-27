d7bdb8d7dc12f5d3ab47e26f8b1aaefe58f176bb
kostovster <kostovster@gmail.com>
2025-03-27 11:58:10 +0200

    update meta

--- src/router/metadata/homepage.ts
+++ src/router/metadata/homepage.ts
@@ -30,16 +30,5 @@ export const META = {
       "https://twitter.com/NolusProtocol",
       "https://www.reddit.com/r/nolusprotocol/"
     ]
-  },
-  "script-video-object": {
-    "@context": "http://schema.org/",
-    "@type": "VideoObject",
-    name: "Nolus defines a money market between lenders looking to yield on stablecoins and borrowers looking to amplify their holdings. For borrowers, the Nolus DeFi Lease provides up to 150% financing on the initial investment with a lowered margin call risk and access to the underlying leveraged assets through whitelisted strategies. Inspired by traditional lease products where one can obtain a physical good by providing only a fraction of the cost upfront and use it throughout the lease duration, the Nolus DeFi Lease unlocks the full potential of crypto money markets by reducing the industry’s steep over-collateralization requirements, resulting in significantly improved capital efficiency",
-    uploadDate: "2022-10-24",
-    duration: "PT0H1M47S",
-    contentUrl: "https://www.youtube.com/watch?v=k7QWPR0052g",
-    embedUrl: "https://www.youtube.com/embed/k7QWPR0052g",
-    thumbnailUrl:
-      "https://i9.ytimg.com/vi/k7QWPR0052g/mqdefault.jpg?v=635641f9&sqp=CPC145oG&rs=AOn4CLBNhkTA4nD9T6Pb9_Jmi4UfQeQzsg"
   }
 };
