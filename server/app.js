f2f5aea850e5e97f8531c1bea2b9ea40deea6b57
alexandermake <alexanderm@prime-code.com>
2025-05-20 09:35:15 +0300

    update(cors)

--- server/app.js
+++ server/app.js
@@ -3,20 +3,11 @@ import express, { json } from "express";
 import { router as medium } from "./medium.js";
 import { router as skip } from "./skip.js";
 
-const origins = ["http://localhost:8080", "http://127.0.0.1:8080", "https://app-dev.nolus.io", "https://app.nolus.io", "https://nolus.io", "https://web-dev.nolus.io"];
 const PORT = process.env.PORT || 5051;
 const app = express();
-const corsOptions = {
-  origin: (origin, next) => {
-    if (origins.includes(origin)) {
-      return next(null, origin);
-    }
-    return next("Cross-Origin Request Blocked")
-  }
-};
 
 app.use(json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/", medium);
 app.use("/skip", skip);
 
